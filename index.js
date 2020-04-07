c676e9b580069adc8687f7f5412e75463601e03d
Ashwani Kumar <kumar.ashwani269@gmail.com>
2020-04-07 19:09:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// This is for the programming language
+let name;
+let age;
+let jobRole = "Quality assurance analyst"
+
+
+function personalData (name,age) {
+    console.log("Hello,I am " + name + " and I am " + age + "years old.")
+}
+
+personalData(Ashwani,28)
+personalData(Ashu,28)
+
+let numberOne;
+let numberTwo;
+
+function addition (numberOne,numberTwo) {
+    let total = numberOne + numberTwo
+    console.log(total)
+
+}
+
+addition(50,40)
+addition(40,80)
+addition(200,890)
+
+
+
+// variables = storage boxes to store the data & we use "let" to use a variable
